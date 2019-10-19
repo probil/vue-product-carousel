2528d9fb189b0e2d607098de12d593973fd1e056
Max Lyashuk <m_lyashuk@ukr.net>
2019-10-19 13:30:09 +0300

    feat(bundler): include global polyfills for app but not library

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
+const isLib = process.env.VUE_CLI_BUILD_TARGET === 'lib';
+
 module.exports = {
   presets: [
-    ['@vue/app', {
-      useBuiltIns: false,
+    ['@vue/cli-plugin-babel/preset', {
+      useBuiltIns: isLib
+        ? false
+        : 'usage',
     }],
   ],
 };
